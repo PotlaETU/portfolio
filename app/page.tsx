649cde4b72d31bbfc3702b038869298372020784
AnthonyR <anthony156gamer@gmail.com>
2024-12-02 21:57:29 +0100

    feat: begin home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
     return (
         <>
-            <main>
-                <p>Test</p>
+            <main className="p-8 text-center">
+                <p className="text-4xl">ROLLAND Anthony</p>
+                <h2 className="text-3xl font-bold bg-gradient-to-r
+                    from-green-400 to-blue-500
+                    bg-clip-text text-transparent">
+                    Étudiant en informatique
+                </h2>
             </main>
         </>
     );
