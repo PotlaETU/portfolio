c6952f74d297b8f121cd8258f3070dbe0cdcfc0f
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2024-12-13 15:15:26 +0100

    feat: add some projects

--- datas/projects.ts
+++ datas/projects.ts
@@ -6,8 +6,24 @@ const projects: Project[] = [
         title: "API PicIt",
         description: "API RESTful pour le réseau social PicIt",
         image: "/projects/1/picit.png",
-        tags: ["Java", "Spring Boot", "MongoDB"],
+        tags: ["Java", "Spring Boot", "MongoDB", "Neo4j", "Docker"],
         githubUrl: "https://github.com/PotlaETU/picit-api",
+    },
+    {
+        id: 2,
+        title: "FCrypt",
+        description: "Outil de chiffrement de fichiers (texte, image, audio, vidéo)",
+        image: "/projects/2/art_project_2.png",
+        tags: ["Spring Shell", "Java", "Chiffrement", "Docker"],
+        githubUrl: "https://github.com/PotlaETU/Fcrpyt",
+    },
+    {
+        id: 3,
+        title: "ClearLinks",
+        description: "Contrats sur la blockchain Ethereum (Sepolia Testnet) permettant de faire des donations à des créateurs, des votes, etc .",
+        image: "/projects/3/clear_links.png",
+        tags: ["TypeScript", "Solidity", "Ethereum", "Hardat"],
+        githubUrl: "https://github.com/PotlaETU/ClearLinks-Contracts"
     }
 ]
 
